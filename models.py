1329939cd9635fe9e27bb2681c9934880322fad0
clarspau <125636195+clarspau@users.noreply.github.com>
2023-12-05 22:51:12 -0800

    Modified models.py to add class methods in registering a user and to authenticate.

--- models.py
+++ models.py
@@ -32,3 +32,36 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.column(db.String(30), nullable=False)
     last_name = db.column(db.String(30), nullable=False)
+
+    # Class methods for user registration and authentication
+
+    @classmethod
+    def register(cls, username, password, first_name, last_name, email):
+        """Register a new user by securely hashing their password."""
+
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        user = cls(
+            username=username,
+            password=hashed_utf8,
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Authenticate a user by checking the provided username and password.
+
+        Return user if valid; else return False.
+        """
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
