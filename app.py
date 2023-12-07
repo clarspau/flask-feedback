08c6754c1e82fcf337a0a494512a46a28a0ff9f8
clarspau <125636195+clarspau@users.noreply.github.com>
2023-12-06 21:17:05 -0800

    Created register.html and forms.py, and modified app.py to make routes for registering a user.

--- app.py
+++ app.py
@@ -1,3 +1,57 @@
 """flask Feedback app."""
 
 from flask import Flask, render_template, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db, db, User
+from forms import RegisterForm
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask-feedback"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "Secret Feedback"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+
+@app.route("/")
+def homepage():
+    """Homepage.
+    Redirect to /register.
+    """
+
+    return redirect("/register")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register a new user.
+    Provide a form and handle form submission.
+    """
+
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+
+        user = User.register(username, password, first_name, last_name, email)
+
+        db.session.commit()
+        session['username'] = user.username
+
+        return redirect(f"/users/{user.username}")
+
+    else:
+        return render_template("users/register.html", form=form)
